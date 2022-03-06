4ee0d00f9fa7dcb74075aa6d40f46c5e4dca25a7
Kesava Kadiyala <kesavakadiyala@gmail.com>
2022-03-06 08:32:01 +0530

    Bug Fixes

--- vars.tf
+++ vars.tf
@@ -2,8 +2,3 @@ variable "component" {}
 variable "bucket" {}
 variable "ENV" {}
 variable "availability-zones" {}
-variable "PAT" {}
-PAT = "${locals.PAT}"
-locals {
-  PAT = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)["PAT"]
-}
